97f6670736ed7db2455484f2f12d29d0123c6d0c
“Mit <mitpatel23897@gmail.com>
2020-01-30 19:33:05 +0530

    Grid-list basic model added

--- home/home.controller.js
+++ home/home.controller.js
@@ -15,17 +15,20 @@
         vm.deleteUser = deleteUser;
 
 
-        $scope.datatype = "false"
+        $scope.datatype = "false";
         $scope.displabel = "Grid";
 
         $scope.onChange = function (cbState) {
             $scope.datatype = cbState;
-            if($scope.datatype)
-            {
-                $scope.displabel="List"
+            if ($scope.datatype) {
+                $scope.displabel = "List";
+                console.log("List clicked");
+                loadInList();
             }
-            else{
-                $scope.displabel="Grid"
+            else {
+                $scope.displabel = "Grid";
+                console.log("Grid clicked");
+                loadInGrid();
             }
         };
 
@@ -46,39 +49,77 @@
                 .then(function () {
                     console.log(vm.user.city);
                     // console.log(vm.user.city[]);
-                    $scope.cities = [];
-
-                    var i = 0;
-
-                    let arr = [];
-
-                    while (vm.user.city[i]) {
-                        console.log(vm.user.city[i]);
-
-                        //console.log("http://api.openweathermap.org/data/2.5/weather?q=" + vm.user.city[i] + "&APPID=e4fbbdc28e9c62296fed91870dfc65dc");
-                        let weturl = "http://api.openweathermap.org/data/2.5/weather?q=" + vm.user.city[i] + "&APPID=e4fbbdc28e9c62296fed91870dfc65dc";
-
-                        let wetdata = [];
-
-                        fetch(weturl)
-                            .then((response) => {
-                                return response.json();
-                            })
-                            .then((wetdata) => {
-                                //console.log(wetdata);
-                                // JSON.stringify(wetdata);
-                                console.log(wetdata);
-                                arr.push(wetdata);
-                                console.log(arr);
-                                loadAllUsers();
-                            });
-                        i++;
-                    }
-                    $scope.cities = arr;
+                    loadInGrid();
                 });
         }
 
 
+        function loadInGrid() {
+            $scope.cities = [];
+
+            var i = 0;
+
+            let arr = [];
+
+            while (vm.user.city[i]) {
+                console.log(vm.user.city[i]);
+
+                //console.log("http://api.openweathermap.org/data/2.5/weather?q=" + vm.user.city[i] + "&APPID=e4fbbdc28e9c62296fed91870dfc65dc");
+                let weturl = "http://api.openweathermap.org/data/2.5/weather?q=" + vm.user.city[i] + "&APPID=e4fbbdc28e9c62296fed91870dfc65dc";
+
+                let wetdata = [];
+
+                fetch(weturl)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((wetdata) => {
+                        //console.log(wetdata);
+                        // JSON.stringify(wetdata);
+                        console.log(wetdata);
+                        arr.push(wetdata);
+                        console.log(arr);
+                        loadAllUsers();
+                    });
+                i++;
+            }
+            $scope.cities = arr;
+        }
+
+
+        function loadInList() {
+            $scope.CTS = [];
+
+            var i = 0;
+
+            let arr = [];
+
+            while (vm.user.city[i]) {
+                console.log("   LIT");
+
+                //console.log("http://api.openweathermap.org/data/2.5/weather?q=" + vm.user.city[i] + "&APPID=e4fbbdc28e9c62296fed91870dfc65dc");
+                let weturl = "http://api.openweathermap.org/data/2.5/weather?q=" + vm.user.city[i] + "&APPID=e4fbbdc28e9c62296fed91870dfc65dc";
+
+                let wetdata = [];
+
+                fetch(weturl)
+                    .then((response) => {
+                        return response.json();
+                    })
+                    .then((wetdata) => {
+                        //console.log(wetdata);
+                        // JSON.stringify(wetdata);
+                        console.log(wetdata);
+                        arr.push(wetdata);
+                        console.log(arr);
+                        loadAllUsers();
+                    });
+                i++;
+            }
+            $scope.CTS = arr;
+
+
+        }
 
         function loadAllUsers() {
             UserService.GetAll()
